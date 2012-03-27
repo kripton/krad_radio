524d9f0b6611ef606b19b0f0ded7225c89a831b2
David Richards <rawdod@gmail.com>
2012-03-27 13:32:58 -0400

    quickdemo kludgefix

--- krad_tools/krad_link/krad_link.c
+++ krad_tools/krad_link/krad_link.c
@@ -2531,9 +2531,9 @@ int krad_linker_handler ( krad_linker_t *krad_linker, krad_ipc_server_t *krad_ip
 					krad_link->video_codec = NOCODEC;
 					krad_link->operation_mode = CAPTURE;
 					strncpy (krad_link->password, "secretkode", sizeof(krad_link->password));
-					krad_link->tcp_port = 8080;
+					krad_link->tcp_port = 9080;
 					strcpy (krad_link->host, "kradradio.com");
-					strcpy (krad_link->mount, "/krdo.webm");
+					strcpy (krad_link->mount, "/mixtest.webm");
 					krad_link_run (krad_link);
 					break;
 				}
