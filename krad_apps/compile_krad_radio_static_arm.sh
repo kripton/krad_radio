d6a03008a9eee2d68cb479d3050fb4cc8279ff59
David Richards <rawdod@gmail.com>
2012-05-10 09:11:40 -0400

    dust bunny gone

--- krad_apps/compile_krad_radio_static_arm.sh
+++ krad_apps/compile_krad_radio_static_arm.sh
@@ -1,15 +1,3 @@
-#echo "----krad radio -----"
-
-#gcc -Wall -g -pthread krad_radio.c ../krad_tools/krad_radio/krad_radio.c ../krad_tools/krad_ipc/krad_ipc_server.c \
-#-I../krad_tools/krad_ipc ../krad_tools/krad_ipc/krad_ipc_client.c -I../krad_tools/krad_web/ -I../krad_tools/krad_radio \
-#../krad_ebml/krad_ebml.c -I../krad_ebml/ -I../krad_tools/krad_link \
-#../krad_tools/krad_mixer/krad_mixer.c -I../krad_tools/krad_mixer/ \
-#../krad_tools/krad_effects/util/rms.c ../krad_tools/krad_effects/util/db.c ../krad_tools/krad_effects/digilogue.c ../krad_tools/krad_effects/djeq.c ../krad_tools/krad_effects/fastlimiter.c \
-#../krad_tools/krad_effects/sidechain_comp.c ../krad_tools/krad_effects/hardlimiter.c -I../krad_tools/krad_effects/ -I../krad_tools/krad_effects/util \
-#../krad_tools/krad_tags/krad_tags.c -I../krad_tools/krad_tags/ \
-#../krad_tools/krad_web/krad_websocket.c ../krad_tools/krad_web/krad_http.c -o krad_radio -lrt -lwebsockets -lm -ljack
-
-
 echo "----krad radio -----"
 
 cd ../
@@ -110,10 +98,6 @@ gcc -g -Wall krad_radio.c \
 -o krad_radio `pkg-config --cflags --libs gtk+-3.0 xcb x11 gl xext xcb-util xcb-aux xcb-atom cairo` \
 -lX11-xcb -lrt -lxcb-image -lm -ljack -lpulse -lasound -lcodec2 -lsamplerate -lwebsockets
 
-#-lXext -lX11 -lXmu -lXi -lGL -lGLU -lm -ljack -lpulse -lasound
-
-
-
 echo "----krad radio cmd -----"
 
 gcc -Wall -pthread krad_radio_cmd.c ../krad_tools/krad_ipc/krad_ipc_client.c \
@@ -121,6 +105,4 @@ gcc -Wall -pthread krad_radio_cmd.c ../krad_tools/krad_ipc/krad_ipc_client.c \
 
 
 sh compile_krad_radio_gtk_static.sh
-#gcc -Wall -pthread -g krad_radio_gtk.c ../krad_tools/krad_ipc/krad_ipc_client.c \
-#-I../krad_tools/krad_ipc -I../krad_tools/krad_radio ../krad_ebml/krad_ebml.c -I../krad_ebml/ -o krad_radio_gtk \
-#`pkg-config --cflags --libs gtk+-3.0` -lrt -lm
+
