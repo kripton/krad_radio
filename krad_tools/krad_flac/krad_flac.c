356372e527c2788ce449eb0f99914cc020b39758
David Richards <rawdod@gmail.com>
2012-05-27 19:09:36 -0400

    fix misnumbering

--- krad_tools/krad_flac/krad_flac.c
+++ krad_tools/krad_flac/krad_flac.c
@@ -146,9 +146,9 @@ FLAC__StreamEncoderWriteStatus krad_flac_encoder_write_callback (
 		flac->comment = calloc (1, 4 + op.bytes);		
 		// 0x84 = final metadata block and is a vorbis comment
 		flac->comment[0] = 0x84;
+		flac->comment[1] = 0x00;
 		flac->comment[2] = 0x00;
-		flac->comment[3] = 0x00;
-		flac->comment[4] = op.bytes;
+		flac->comment[3] = op.bytes;
 		
 		memcpy (flac->comment + 4, op.packet, op.bytes);
 		
