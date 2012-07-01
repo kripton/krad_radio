c6baeb72b060d30042ca2df9a9130bbd03eeeae2
David Richards <rawdod@gmail.com>
2012-07-01 16:18:44 -0400

    fix ktv script

--- scripts/ktv.rb
+++ scripts/ktv.rb
@@ -3,7 +3,7 @@
 require_relative "kradradio.rb"
 
 station_name = "ktv1"
-playlist_dir = "/data2/youtube"
+playlist_dir = "/data2/youtube/amp"
 server1 = "phobos.kradradio.com 8088"
 server2 = "phobos.kradradio.com 8080"
 
@@ -18,7 +18,7 @@ puts station.uptime
 
 station.set_resolution(width, height)
 
-station.sprite("~/Pictures/kr3.png", 470, 404)
+station.sprite("#{File.expand_path('~')}/Pictures/kr3.png", 470, 404)
 sleep 0.2
 station.cmd("transmit audiovideo #{server1} /ktv.webm secretkode null #{width} #{height} #{bitrate}")
 sleep 0.2
