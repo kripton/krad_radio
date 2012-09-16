7d2c449392d4e483bba8754f30f93695183b1cd8
David Richards <drichards@mozilla.com>
2012-09-16 02:15:30 -0700

    add mixer common struct file.. not yet used

---
+++ tools/krad_mixer/krad_mixer_common.h
@@ -0,0 +1,16 @@
+
+typedef struct krad_mixer_portgroup_rep_St krad_mixer_portgroup_rep_t;
+
+#ifndef KRAD_MIXER_COMMON_H
+#define KRAD_MIXER_COMMON_H
+
+struct krad_mixer_portgroup_rep_St {
+
+	char sysname[256];
+
+	int xmms2;
+
+};
+
+
+#endif
