37cc0798ddc1f3630d02a97c06f22c466a0a0462
David Richards <drichards@mozilla.com>
2012-12-01 23:38:21 -0800

    actually include imagezero in-tree for krad vhs codec

---
+++ lib/krad_vhs/ext/imagezero/libiz.cpp
@@ -0,0 +1,33 @@
+#include "libiz.h"
+#include "encoder.h"
+#include "decoder.h"
+
+namespace IZ {
+
+unsigned char *encodeImage(const Image<> &im, unsigned char *dest)
+{
+    ImageEncoder<> ic;
+    ic.begin(dest);
+    ic.encodeImageSize(im);
+    ic.encodeImagePixels(im);
+    return ic.end();
+}
+
+const unsigned char *decodeImage(Image<> &im, const unsigned char *src)
+{
+    ImageDecoder<> ic;
+    ic.begin(src);
+    ic.skipImageSize();
+    ic.decodeImagePixels(im);
+    return ic.end();
+}
+
+void decodeImageSize(Image<> &im, const unsigned char *src)
+{
+    ImageDecoder<> ic;
+    ic.begin(src);
+    ic.decodeImageSize(im);
+    ic.end();
+}
+
+} // namespace IZ
